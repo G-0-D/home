e9b48f507e22444db5028ca8c44915ba9ca508fc
G-0-D <39115725+G-0-D@users.noreply.github.com>
2023-12-06 23:14:17 +0900

    Update getTime.js

--- src/utils/getTime.js
+++ src/utils/getTime.js
@@ -78,21 +78,21 @@ export const helloInit = () => {
   } else if (hour < 9) {
     hello = "早上好";
   } else if (hour < 12) {
-    hello = "上午好";
+    hello = "Good morning";
   } else if (hour < 14) {
-    hello = "中午好";
+    hello = "Good afternoon";
   } else if (hour < 17) {
-    hello = "下午好";
+    hello = "Good afternoon";
   } else if (hour < 19) {
     hello = "傍晚好";
   } else if (hour < 22) {
-    hello = "晚上好";
+    hello = "good evening";
   } else {
     hello = "夜深了";
   }
   ElMessage({
     dangerouslyUseHTMLString: true,
-    message: `<strong>${hello}</strong> 欢迎来到我的主页`,
+    message: `<strong>${hello}</strong> welcome to ARVAS`,
   });
 };
 
